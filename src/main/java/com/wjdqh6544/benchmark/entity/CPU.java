c47caa2773fa778d8d4ba8c2b2246f750652a0c9
HyeongCheol Seo <wjdqh6544@naver.com>
2024-08-01 19:47:21 +0900

    Implement entity classes. - CPU / Column: ID(PK), CPUName(Unique), cineBenchR23MT, cineBenchR23ST - GPU / Column: ID(PK), GPUName(Unique), timeSpy
    Benchmark platform will be added if needed.

--- src/main/java/com/wjdqh6544/benchmark/entity/CPU.java
+++ src/main/java/com/wjdqh6544/benchmark/entity/CPU.java
@@ -1,11 +1,26 @@
 package com.wjdqh6544.benchmark.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 /*
 BLOG_Benchmark_Data_Parser_with_JSON
 - entity/CPU: an entity class that connect with "CPU" table of Database
 */
 @Entity
+@Getter
+@RequiredArgsConstructor
 public class CPU {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String cpuName;
+
+    @Column(name = "cinebench_r23_mt")
+    private Integer cinebenchR23MT;
+
+    @Column(name = "cinebench_r23_st")
+    private Integer cinebenchR23ST;
 }
--- src/main/java/com/wjdqh6544/benchmark/entity/GPU.java
+++ src/main/java/com/wjdqh6544/benchmark/entity/GPU.java
@@ -1,11 +1,23 @@
 package com.wjdqh6544.benchmark.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 /*
 BLOG_Benchmark_Data_Parser_with_JSON
 - entity/GPU: an entity class that connect with "GPU" table of Database
 */
 @Entity
+@RequiredArgsConstructor
+@Getter
 public class GPU {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String gpuName;
+
+    @Column(name = "timespy")
+    private Integer timeSpy;
 }
