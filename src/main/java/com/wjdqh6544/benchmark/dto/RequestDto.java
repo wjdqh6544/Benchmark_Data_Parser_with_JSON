f292bc21ed36df4cf6b55d11d8a2e81f8948de4a
HyeongCheol Seo <wjdqh6544@naver.com>
2024-08-04 18:04:39 +0900

    Tested and debugged.
    - Add application.yml (DB Connection, Log Setting, Swagger Setting, and so on.)
    - RequestDto: Add Constructor and apply ConstructorProperties annotation
    - Add exception for "benchmark" field is null or empty.
    - Edit Return JSON Type.
    - Fix a problem that JSON was not reset when exception invoked.

--- src/main/java/com/wjdqh6544/benchmark/dto/RequestDto.java
+++ src/main/java/com/wjdqh6544/benchmark/dto/RequestDto.java
@@ -1,8 +1,8 @@
 package com.wjdqh6544.benchmark.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.beans.ConstructorProperties;
 import java.util.List;
 
 /*
@@ -10,8 +10,17 @@ BLOG_Benchmark_Data_Parser_with_JSON
 - dto/RequestDto: a DTO class that transfers request parameter to service class.
 */
 @Getter
-@AllArgsConstructor
 public class RequestDto {
     private String benchmark;
     private List<String> productList;
+
+    @ConstructorProperties({"benchmark", "name"})
+    public RequestDto(String benchmark, List<String> productList){
+        if (benchmark != null){
+            this.benchmark = benchmark.replace(" ", "");
+        } else {
+            this.benchmark = null;
+        }
+        this.productList = productList;
+    }
 }
--- src/main/java/com/wjdqh6544/benchmark/exception/NotFoundException.java
+++ src/main/java/com/wjdqh6544/benchmark/exception/NotFoundException.java
@@ -2,9 +2,8 @@ package com.wjdqh6544.benchmark.exception;
 
 import com.wjdqh6544.benchmark.vo.GetEachResultVo;
 import com.wjdqh6544.benchmark.vo.GetResultListVo;
-
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /*
 BLOG_Benchmark_Data_Parser_with_JSON
@@ -12,23 +11,35 @@ BLOG_Benchmark_Data_Parser_with_JSON
 */
 public class NotFoundException {
     private static GetEachResultVo eachResult;
-    private static ArrayList<GetEachResultVo> resList = new ArrayList<>();
+    private static ArrayList<GetEachResultVo> resList;
     public static GetResultListVo productListIsEmpty(String productType){
         eachResult = GetEachResultVo.builder()
                 .productName("400 Bad Request. / List of requested" + productType + " is empty.")
                 .score(-1)
                 .build();
-        resList.add(eachResult);
+        resList = new ArrayList<>(Arrays.asList(eachResult));
         return GetResultListVo.builder()
                 .resultVoList(resList)
                 .build();
     }
-    public static GetResultListVo benchmarkNotFound(String benchName){
+
+    public static GetResultListVo benchmarkIsEmpty(){
         eachResult = GetEachResultVo.builder()
-                .productName("404 Not Found. / " + benchName + " Not Found.")
+                .productName("400 Bad Request. / Benchmark type is empty.")
                 .score(-1)
                 .build();
-        resList.add(eachResult);
+        resList = new ArrayList<>(Arrays.asList(eachResult));
+        return GetResultListVo.builder()
+                .resultVoList(resList)
+                .build();
+    }
+
+    public static GetResultListVo benchmarkNotFound(String benchName){
+        eachResult = GetEachResultVo.builder()
+                .productName("404 Not Found. / Benchmark(" + benchName + ") Not Found.")
+                .score(-1)
+                .build();
+        resList = new ArrayList<>(Arrays.asList(eachResult));
         return GetResultListVo.builder()
                 .resultVoList(resList)
                 .build();
@@ -36,7 +47,7 @@ public class NotFoundException {
 
     public static GetEachResultVo productNotFound(String productType, String productName) {
         return GetEachResultVo.builder()
-                .productName("404 Not Found. / " + productType + " - " + productName)
+                .productName("404 Not Found. / " + productType + "(" + productName + ") Not Found.")
                 .score(-1)
                 .build();
     }
